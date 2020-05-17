6ff9e4ecf6a285d25618e4431a78b46346311260
Gabe Fierro <gtfierro@cs.berkeley.edu>
2020-05-17 13:07:38 -0600

    environment var-based configuration

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,5 +1,9 @@
 package config
 
+import (
+	"os"
+)
+
 type Config struct {
 	GRPC     GRPC
 	HTTP     HTTP
@@ -22,4 +26,24 @@ type GRPC struct {
 type HTTP struct {
 	ListenAddress string
 	Port          string
+}
+
+func NewFromEnv() *Config {
+	return &Config{
+		GRPC: GRPC{
+			ListenAddress: os.Getenv("COMMONS_GRPC_ADDRESS"),
+			Port:          os.Getenv("COMMONS_GRPC_PORT"),
+		},
+		HTTP: HTTP{
+			ListenAddress: os.Getenv("COMMONS_HTTP_ADDRESS"),
+			Port:          os.Getenv("COMMONS_HTTP_PORT"),
+		},
+		Database: Database{
+			Host:     os.Getenv("COMMONS_DB_HOST"),
+			Database: os.Getenv("COMMONS_DB_DATABASE"),
+			User:     os.Getenv("COMMONS_DB_USER"),
+			Password: os.Getenv("COMMONS_DB_PASSWORD"),
+			Port:     os.Getenv("COMMONS_DB_PORT"),
+		},
+	}
 }
